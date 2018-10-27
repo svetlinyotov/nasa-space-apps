c230e98316b23b08dbbe083910537518319aa0d5
shefasf <bobko_2000@abv.bg>
2018-10-27 18:35:36 +0300

    Adding preference.

---
+++ app/src/main/java/com/snsdevelop/nasa/disaster/PreferenceActivity.java
@@ -0,0 +1,23 @@
+package com.snsdevelop.nasa.disaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.snsdevelop.nasa.disaster.R;
+
+public class PreferenceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preference);
+
+
+
+        //buttonListAll.setOnClickListener(view -> {
+          //  Intent myIntent = new Intent(this, StepsListActivity.class);
+            //startActivity(myIntent);
+        //});
+
+    }
+}
